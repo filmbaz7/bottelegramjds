32cfcd0945bd3bfb4264a6f9646931d35acbb4cf
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-23 01:30:33 -0700

    Update db.py

--- jdscraper/db.py
+++ jdscraper/db.py
@@ -1,24 +1,20 @@
-
-import sqlite3
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "products.db")
-
-def get_connection():
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            priceWas REAL,
-            priceIs REAL,
-            difference REAL,
-            discount REAL,
-            link TEXT UNIQUE,
-            image TEXT
-        )
-    ''')
+def create_tables():
+    conn = sqlite3.connect("products.db")
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        priceWas REAL,
+        priceIs REAL,
+        difference REAL,
+        discount REAL,
+        link TEXT UNIQUE,
+        image TEXT
+    )''')
+    
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        chat_id TEXT PRIMARY KEY
+    )''')
     conn.commit()
-    return conn
+    conn.close()
